7f560db62e1c590bfe8d63b09c469fd5afa69d96
Patrizio Cannoni <patrizio.cannoni@gmail.com>
2025-04-05 09:13:47 -0300

    Formatação de arquivos

--- src/main/java/br/com/demo/eventhub/EventhubApplication.java
+++ src/main/java/br/com/demo/eventhub/EventhubApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EventhubApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EventhubApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EventhubApplication.class, args);
+    }
 
 }
--- src/test/java/br/com/demo/eventhub/EventhubApplicationTests.java
+++ src/test/java/br/com/demo/eventhub/EventhubApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EventhubApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
